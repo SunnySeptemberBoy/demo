f775685eb6f254dc1e7dc85cd4ed62061da9d590
basic <643017440@qq.com>
2024-07-10 23:32:48 +0800

    test: 2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <script setup>
 import { ref } from 'vue'
 
-const test = ref('')
-
 const fileInput = ref(null)
-const show = ref(false)
-const fileName = ref('')
-const actions = [
-  { name: '相册', key: 'picture' },
-  { name: '相机', key: 'graph' }
-]
+const url = ref('')
+// const show = ref(false)
+// const fileName = ref('')
+// const actions = [
+//   { name: '相册', key: 'picture' },
+//   { name: '相机', key: 'graph' }
+// ]
 
 const handleFileInputChange = (e) => {
   fileName.value = e.target.files[0].name
   const url = URL.createObjectURL(this.files[0])
+  url.value = url
   console.log('🚀 ~ handleFileInputChange ~ url:', url)
 }
 
-const onSelect = (item) => {
-  // 默认情况下点击选项时不会自动收起
-  // 可以通过 close-on-click-action 属性开启自动收起
-  show.value = false
-  console.log(item)
-  const { key } = item
-  if (key === 'picture') {
-    fileInput.value.type = 'file'
-  } else {
-    fileInput.value.type = 'text'
-  }
+// const onSelect = (item) => {
+//   // 默认情况下点击选项时不会自动收起
+//   // 可以通过 close-on-click-action 属性开启自动收起
+//   show.value = false
+//   console.log(item)
+//   const { key } = item
+//   if (key === 'picture') {
+//     fileInput.value.type = 'file'
+//   } else {
+//     fileInput.value.type = 'text'
+//   }
+//   fileInput.value.click()
+// }
+const handleFileUpload = () => {
   fileInput.value.click()
 }
 </script>
@@ -36,10 +39,11 @@ const onSelect = (item) => {
   <input
     v-show="false"
     ref="fileInput"
-    type="text"
+    type="file"
     accept="image/*"
     @change="handleFileInputChange"
   />
-  <van-cell is-link title="基础用法" @click="show = true" />
-  <van-action-sheet v-model:show="show" :actions="actions" @select="onSelect" />
+  <button @click="handleFileUpload">森分钟</button>
+  <!-- <van-cell is-link title="基础用法" @click="show = true" />
+  <van-action-sheet v-model:show="show" :actions="actions" @select="onSelect" /> -->
 </template>
